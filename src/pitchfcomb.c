f97445ca7bef8ecc309b272684a6c174747b9774
Paul Brossier <piem@altern.org>
2005-08-22 19:33:30 +0000

    fcomb doesn't output anything correct above 5000Hz

--- src/pitchfcomb.c
+++ src/pitchfcomb.c
@@ -120,7 +120,7 @@ smpl_t aubio_pitchfcomb_detect (aubio_pitchfcomb_t * p, fvec_t * input)
     }
   }
   /* quick hack to clean output a bit */
-  if (peaks[k].freq > 10000) return 0.;
+  if (peaks[k].freq > 5000.) return 0.;
   return peaks[k].freq;
 }
 
