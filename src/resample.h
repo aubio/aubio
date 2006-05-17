ca29f57118ee693cbb9078ebd48efc648535d20f
Paul Brossier <piem@altern.org>
2006-05-17 19:46:52 +0000

    update resample.h docs update resample.h docs

--- src/resample.h
+++ src/resample.h
@@ -20,32 +20,37 @@
 #ifndef _RESAMPLE_H
 #define _RESAMPLE_H
 
-/** @file
- * Libsamplerate resampling
- */
+/** \file
+ 
+ Resampling object
+
+ This object resamples an input vector into an output vector using
+ libsamplerate. See http://www.mega-nerd.com/SRC/
+ 
+*/
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/** 
- * resampler object
- */
+/** resampler object */
 typedef struct _aubio_resampler_t aubio_resampler_t;
-/**  
- * create resampler object 
- * \param ratio output_sample_rate / input_sample_rate 
- * \param type libsamplerate resampling type
- */
+/** create resampler object 
+
+  \param ratio output_sample_rate / input_sample_rate 
+  \param type libsamplerate resampling type
+
+*/
 aubio_resampler_t * new_aubio_resampler(float ratio, uint_t type);
 /** delete resampler object */
 void del_aubio_resampler(aubio_resampler_t *s);
-/** 
- * resample input in output
- * \param s resampler object
- * \param input input buffer of size N
- * \param output output buffer of size N*ratio
- */
+/** resample input in output
+
+  \param s resampler object
+  \param input input buffer of size N
+  \param output output buffer of size N*ratio
+
+*/
 uint_t aubio_resampler_process(aubio_resampler_t *s, fvec_t * input,  fvec_t * output);
 
 #ifdef __cplusplus
