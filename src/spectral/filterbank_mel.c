1c2e186070986bacc1b08fdaaa3bb559f534af58
Paul Brossier <piem@piem.org>
2009-09-26 00:32:31 +0200

    src/spectral/ update license headers

--- src/spectral/filterbank_mel.c
+++ src/spectral/filterbank_mel.c
@@ -2,20 +2,20 @@
   Copyright (C) 2007-2009 Paul Brossier <piem@aubio.org>
                       and Amaury Hazan <ahazan@iua.upf.edu>
 
-  This file is part of Aubio.
+  This file is part of aubio.
 
-  Aubio is free software: you can redistribute it and/or modify
+  aubio is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  Aubio is distributed in the hope that it will be useful,
+  aubio is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with Aubio.  If not, see <http://www.gnu.org/licenses/>.
+  along with aubio.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
--- src/spectral/mfcc.h
+++ src/spectral/mfcc.h
@@ -1,21 +1,21 @@
 /*
-   Copyright (C) 2007-2009 Paul Brossier <piem@aubio.org>
-                       and Amaury Hazan <ahazan@iua.upf.edu>
+  Copyright (C) 2007-2009 Paul Brossier <piem@aubio.org>
+                      and Amaury Hazan <ahazan@iua.upf.edu>
 
-   This file is part of Aubio.
+  This file is part of aubio.
 
-   Aubio is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+  aubio is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-   Aubio is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+  aubio is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with Aubio.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with aubio.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
