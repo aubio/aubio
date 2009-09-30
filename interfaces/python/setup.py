0a6c2115562490aff6ef173bff92e28e48444e85
Paul Brossier <piem@piem.org>
2009-09-30 03:46:35 +0200

    added new python interface draft, work in progress

---
+++ interfaces/python/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup, Extension
+
+setup(name="_aubio", version="1.0",
+      ext_modules = [ 
+        Extension("_aubio",
+            ["aubiomodule.c"],
+            include_dirs=['../../build/default/src', '../../src' ],
+            library_dirs=['../../build/default/src', '../../src/.libs' ],
+            libraries=['aubio'])])
+
