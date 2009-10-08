874ed554c0c2e670e329ac34691614d7a8710454
Paul Brossier <piem@piem.org>
2009-10-08 04:24:19 +0200

    tests/python/template.py: factorise

--- tests/python/template.py
+++ tests/python/template.py
@@ -1,4 +1,5 @@
 import unittest
+from commands import getstatusoutput
 from numpy import array
 
 class aubio_unit_template(unittest.TestCase):
@@ -17,6 +18,22 @@ class aubio_unit_template(unittest.TestCase):
       if round((second-first)/first, places) != 0:
         raise self.failureException, \
               (msg or '%r != %r within %r places' % (first, second, places))
+
+class program_test_case(unittest.TestCase):
+
+  filename = "/dev/null"
+  progname = "UNDEFINED"
+  command = ""
+  options = ""
+
+  def getOutput(self, expected_status = 0):
+    self.command = self.progname + ' -i ' + self.filename + self.command
+    self.command += self.options
+    [self.status, self.output] = getstatusoutput(self.command)
+    if expected_status != -1:
+      assert self.status == expected_status, \
+        "expected status was %s, got %s\nOutput was:\n%s" % \
+        (expected_status, self.status, self.output)
 
 def array_from_text_file(filename, dtype = 'float'):
   return array([line.split() for line in open(filename).readlines()], 
