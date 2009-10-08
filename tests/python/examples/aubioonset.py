ca559548b401e0512f84d5a37206e99d73aea45a
Paul Brossier <piem@piem.org>
2009-10-08 04:31:11 +0200

    tests/python/examples/aubioonset.py: do not check onset is actually detected with all functions

--- tests/python/examples/aubioonset.py
+++ tests/python/examples/aubioonset.py
@@ -1,4 +1,4 @@
-from template import *
+from template import program_test_case
 
 class aubioonset_unit(program_test_case):
   
@@ -6,17 +6,19 @@ class aubioonset_unit(program_test_case):
   filename = os.path.join('..','..','sounds','woodblock.aiff')
   progname = os.path.join('..','..','examples','aubioonset')
 
-  def test_aubioonset(self):
-    """ test aubioonset with default parameters """
-    self.getOutput()
-    assert len(str(self.output)) != 0, "no output produced with command:\n" \
-      + self.command
-
   def test_aubioonset_with_inf_silence(self):
     """ test aubioonset with -s 0  """
     self.command += " -s 0" 
     self.getOutput()
     assert len(self.output) == 0, self.output
+
+class aubioonset_unit_finds_onset(aubioonset_unit):
+
+  def test_aubioonset(self):
+    """ test aubioonset with default parameters """
+    self.getOutput()
+    assert len(str(self.output)) != 0, "no output produced with command:\n" \
+      + self.command
 
   def test_aubioonset_with_no_silence(self):
     """ test aubioonset with -s -100 """ 
