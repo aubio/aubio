b45b4ea3754ac5c5e045bf7b2a4dd9f9a70ac5ff
Paul Brossier <piem@piem.org>
2009-10-08 21:04:33 +0200

    tests/python/localaubio.py: swith location

--- tests/python/localaubio.py
+++ tests/python/localaubio.py
@@ -10,10 +10,10 @@ except ImportError:
     import sys
     cur_dir = os.path.dirname(__file__)
     sys.path.append(os.path.join(cur_dir,'..','..','python'))
-    # waf places
+    # autotools places
     sys.path.append(os.path.join(cur_dir,'..','..','python','aubio'))
     sys.path.append(os.path.join(cur_dir,'..','..','python','aubio','.libs'))
-    # autotools places
+    # waf places
     sys.path.append(os.path.join(cur_dir,'..','..','build', 'default', 'swig'))
     sys.path.append(os.path.join(cur_dir,'..','..','build', 'default', 'python','aubio'))
     from aubiowrapper import * 
