992beaa9bea5d3cd1aae961f80f581d2003bcc79
Paul Brossier <piem@piem.org>
2009-10-21 19:54:58 +0200

    interfaces/python/setup.py: only compile c files

--- interfaces/python/setup.py
+++ interfaces/python/setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup, Extension
 
 from os import listdir
 generated_files = listdir('generated')
+generated_files = filter(lambda x: x.endswith('.c'), generated_files)
 generated_files = ['generated/'+f for f in generated_files]
 
 setup(name="_aubio", version="1.0",
