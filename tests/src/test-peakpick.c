2a109e4ee1f09fe440cd0b776134440ad9dcd6bc
Paul Brossier <piem@piem.org>
2009-12-05 02:16:22 +0100

    tests/src/: no more arguments for peakpicker

--- tests/src/test-peakpick.c
+++ tests/src/test-peakpick.c
@@ -7,7 +7,7 @@ int main(){
         uint_t win_s      = 1024;                       /* window size */
         fvec_t * in       = new_fvec (win_s); /* input buffer */
         fvec_t * out      = new_fvec (1); /* input buffer */
-        aubio_peakpicker_t * o = new_aubio_peakpicker(1);
+        aubio_peakpicker_t * o = new_aubio_peakpicker();
         aubio_peakpicker_set_threshold (o, 0.3);
 
         aubio_peakpicker_do(o, in, out);
