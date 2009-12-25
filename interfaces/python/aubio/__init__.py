d836c21f8aa6eac5751a1e3e88cb9ce9e4350b31
Paul Brossier <piem@piem.org>
2009-12-25 06:13:14 +0100

    aubio/__init__.py: simplify, add support for list input

--- interfaces/python/aubio/__init__.py
+++ interfaces/python/aubio/__init__.py
@@ -3,9 +3,7 @@ from _aubio import *
 
 class fvec(numpy.ndarray):
 
-    def __init__(self, length = 1024, **kwargs):
-        super(numpy.ndarray, self).__init__(**kwargs)
-
     def __new__(self, length = 1024, **kwargs):
-        self = numpy.zeros(length, dtype='float32', **kwargs)
-        return self
+        if type(length) == type([]):
+            return numpy.array(length, dtype='float32', **kwargs)
+        return numpy.zeros(length, dtype='float32', **kwargs)
