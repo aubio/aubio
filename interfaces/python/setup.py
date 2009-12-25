b3130e2c76531d254927bfae6b56c9244dbe9bec
Paul Brossier <piem@piem.org>
2009-12-25 04:51:28 +0100

    setup.py: add aubioproxy.c, remove py-fvec.c and py-fmat.c

--- interfaces/python/setup.py
+++ interfaces/python/setup.py
@@ -9,11 +9,11 @@ setup(name="_aubio", version="1.0",
       ext_modules = [ 
         Extension("_aubio",
             ["aubiomodule.c",
-            "py-fvec.c",
-            "py-fmat.c",
+            "aubioproxy.c",
             "py-cvec.c",
+            # example without macro
             "py-filter.c",
-            # macroised 
+            # macroised
             "py-filterbank.c",
             "py-fft.c",
             "py-phasevoc.c",
