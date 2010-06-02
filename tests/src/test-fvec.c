c251b809bd7dac83d1358fe2cd4d987db01d7f00
Paul Brossier <piem@piem.org>
2010-06-03 01:07:33 +0200

    test-fvec.c: update

--- tests/src/test-fvec.c
+++ tests/src/test-fvec.c
@@ -1,11 +1,21 @@
 #include <aubio.h>
+#include <assert.h>
 
 int main(){
-        /* allocate some memory */
-        uint_t win_s      = 1024;                       /* window size */
-        fvec_t * in       = new_fvec (win_s); /* input buffer */
-        del_fvec(in);
+  uint_t buffer_size = 1024;
+  fvec_t * in = new_fvec (buffer_size);
 
-        return 0;
+  assert( in->length                == buffer_size);
+
+  assert( in->data[0]               == 0);
+  assert( in->data[buffer_size / 2] == 0);
+  assert( in->data[buffer_size - 1] == 0);
+
+  in->data[buffer_size -1 ] = 1;
+  assert( in->data[buffer_size - 1] == 1);
+
+  del_fvec(in);
+
+  return 0;
 }
 
