b84c0c229b6ee7f84ab43aba8e07c4586309d726
Paul Brossier <piem@piem.org>
2012-07-11 13:45:12 -0600

    demo_filterbank.py: added simple demo

--- interfaces/python/demo_filterbank.py
+++ interfaces/python/demo_filterbank.py
@@ -1,12 +1,16 @@
-from aubio import filterbank, fvec
+#! /usr/bin/python
+
+try:
+  from aubio import filterbank, fvec
+except ImportError:
+  import localaubio
+  from aubio import filterbank, fvec
 
 f = filterbank(9, 1024)
 freq_list = [60, 80, 200, 400, 800, 1600, 3200, 6400, 12800, 15000, 24000]
 freqs = fvec(freq_list)
 f.set_triangle_bands(freqs, 48000)
-f.get_coeffs().T
 
 from pylab import loglog, show
 loglog(f.get_coeffs().T, '+-')
 show()
-
