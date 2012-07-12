913ee146178e4ad91943b3f1175ca877988a4926
Paul Brossier <piem@piem.org>
2012-07-12 00:59:31 -0700

    demo_filterbank*.py: improved

---
+++ interfaces/python/demo_filterbank_slaney.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+
+from aubio import filterbank
+from numpy import array, arange, vstack
+
+win_s = 8192
+samplerate = 16000
+
+f = filterbank(40, win_s)
+f.set_mel_coeffs_slaney(samplerate)
+
+from pylab import loglog, title, show, xlim, ylim, xlabel, ylabel
+xlim([0,samplerate / 2])
+print f.get_coeffs().shape
+times = vstack([arange(win_s / 2 + 1) * samplerate / win_s] * 40)
+print times.shape
+loglog(times.T, f.get_coeffs().T, '.-')
+title('Mel frequency bands coefficients')
+xlim([100, 7500])
+ylim([1.0e-3, 2.0e-2])
+xlabel('frequency (Hz)')
+ylabel('amplitude')
+show()
