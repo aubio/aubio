5314432e23a351e56ff5341adb5b6a39265b7342
Paul Brossier <piem@piem.org>
2012-07-27 11:34:40 -0600

    interfaces/python/demo_source.py: use source samplerate

--- interfaces/python/demo_source.py
+++ interfaces/python/demo_source.py
@@ -7,9 +7,10 @@ if __name__ == '__main__':
   if len(sys.argv) < 2:
     print 'usage: %s <inputfile>' % sys.argv[0]
     sys.exit(1)
-  f = source(sys.argv[1], 8000, 256)
+  f = source(sys.argv[1], 1, 256)
+  samplerate = f.get_samplerate()
   total_frames, read = 0, 256
   while read:
     vec, read = f()
     total_frames += read
-  print "read", total_frames / float(f.samplerate), "seconds from", f.uri
+  print "read", total_frames / float(samplerate), "seconds from", f.uri
