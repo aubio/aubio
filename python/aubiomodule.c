cd6fc5a515bfc3fe647d2236c3a2949d86a7d4d5
Paul Brossier <piem@piem.org>
2013-02-28 13:18:11 -0500

    python/: remove NPY_NO_DEPRECATED_API to build anyway

--- python/aubiomodule.c
+++ python/aubiomodule.c
@@ -1,6 +1,6 @@
 #include <Python.h>
 #define PY_ARRAY_UNIQUE_SYMBOL PyArray_API
-#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+//#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <numpy/arrayobject.h>
 
 #include "aubio-types.h"
