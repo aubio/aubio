474a573beece1af02259c2c3f194af2e88c0eafc
Paul Brossier <piem@piem.org>
2013-03-03 14:10:45 -0500

    tests/utils_tests.h: add utils_init_random

--- tests/utils_tests.h
+++ tests/utils_tests.h
@@ -1,7 +1,18 @@
+#include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+#include <assert.h>
 
-#define PRINT_ERR(format, args...)   fprintf(stderr, "AUBIO ERROR: " format , ##args)
+#define PRINT_ERR(format, args...)   fprintf(stderr, "AUBIO-TESTS ERROR: " format , ##args)
 #define PRINT_MSG(format, args...)   fprintf(stdout, format , ##args)
 #define PRINT_DBG(format, args...)   fprintf(stderr, format , ##args)
-#define PRINT_WRN(...)               fprintf(stderr, "AUBIO WARNING: " format, ##args)
+#define PRINT_WRN(format, args...)   fprintf(stderr, "AUBIO-TESTS WARNING: " format, ##args)
+
+void utils_init_random () {
+  time_t now = time(0);
+  struct tm *tm_struct = localtime(&now);
+  int seed = tm_struct->tm_sec;
+  //PRINT_WRN("current seed: %d\n", seed);
+  srandom (seed);
+}
