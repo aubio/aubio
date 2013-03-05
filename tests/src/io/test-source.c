f50e53431107ada627c9fef344fa3a23155368ea
Paul Brossier <piem@piem.org>
2013-03-05 00:33:41 -0500

    tests/src/io/test-source.c: default to source samplerate, add hop_size option, print number of frames read

--- tests/src/io/test-source.c
+++ tests/src/io/test-source.c
@@ -7,14 +7,23 @@ int main (int argc, char **argv)
   if (argc < 2) {
     err = 2;
     PRINT_ERR("not enough arguments\n");
-    PRINT_MSG("usage: %s <source_path> [samplerate]\n", argv[0]);
+    PRINT_MSG("read a wave file as a mono vector\n");
+    PRINT_MSG("usage: %s <source_path> [samplerate] [hop_size]\n", argv[0]);
+    PRINT_MSG("examples:\n");
+    PRINT_MSG(" - read file.wav at original samplerate\n");
+    PRINT_MSG("       %s file.wav\n", argv[0]);
+    PRINT_MSG(" - read file.wav at 32000Hz\n");
+    PRINT_MSG("       %s file.aif 32000\n", argv[0]);
+    PRINT_MSG(" - read file.wav at original samplerate with 4096 blocks\n");
+    PRINT_MSG("       %s file.wav 0 4096 \n", argv[0]);
     return err;
   }
 
-  uint_t samplerate = 32000;
+  uint_t samplerate = 0;
   uint_t hop_size = 256;
   uint_t n_frames = 0, read = 0;
   if ( argc == 3 ) samplerate = atoi(argv[2]);
+  if ( argc == 4 ) hop_size = atoi(argv[3]);
 
   char_t *source_path = argv[1];
 
@@ -29,6 +38,9 @@ int main (int argc, char **argv)
     // fvec_print (vec);
     n_frames += read;
   } while ( read == hop_size );
+
+  PRINT_MSG("read %d frames at %dHz (%d blocks) from %s\n", n_frames, samplerate,
+    n_frames / hop_size, source_path);
 
 beach:
   del_aubio_source (s);
