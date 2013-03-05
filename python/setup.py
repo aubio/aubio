4d3b57325f331103ee55d39fa71c6c483f5bc231
Paul Brossier <piem@piem.org>
2013-03-05 16:23:29 -0500

    setup.py: improve, update MANIFEST.in

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 
 from distutils.core import setup, Extension
+
 import sys
 import os.path
 import numpy
@@ -11,12 +12,17 @@ __version__ = '.'.join \
         ([str(x) for x in [AUBIO_MAJOR_VERSION, AUBIO_MINOR_VERSION, AUBIO_PATCH_VERSION]]) \
         + AUBIO_VERSION_STATUS
 
+
 include_dirs = []
 library_dirs = []
 define_macros = []
+extra_link_args = []
 
 include_dirs += ['ext']
 include_dirs += [ numpy.get_include() ]
+
+if sys.platform.startswith('darwin'):
+    extra_link_args += ['-framework','CoreFoundation', '-framework','AudioToolbox']
 
 output_path = 'gen'
 generated_object_files = []
@@ -37,54 +43,51 @@ if os.path.isfile('../src/aubio.h'):
     library_dirs += ['../build/src']
 
 aubio_extension = Extension("aubio._aubio", [
-            "ext/aubiomodule.c",
-            "ext/aubioproxy.c",
-            "ext/ufuncs.c",
-            "ext/py-cvec.c",
-            # example without macro
-            "ext/py-filter.c",
-            # macroised
-            "ext/py-filterbank.c",
-            "ext/py-fft.c",
-            "ext/py-phasevoc.c",
-            # generated files
-            ] + generated_object_files,
-        include_dirs = include_dirs,
-        library_dirs = library_dirs,
-        define_macros = define_macros,
-        libraries=['aubio'])
-
-if sys.platform.startswith('darwin'):
-        aubio_extension.extra_link_args = ['-framework','CoreFoundation', '-framework','AudioToolbox']
+    "ext/aubiomodule.c",
+    "ext/aubioproxy.c",
+    "ext/ufuncs.c",
+    "ext/py-cvec.c",
+    # example without macro
+    "ext/py-filter.c",
+    # macroised
+    "ext/py-filterbank.c",
+    "ext/py-fft.c",
+    "ext/py-phasevoc.c",
+    # generated files
+    ] + generated_object_files,
+    include_dirs = include_dirs,
+    library_dirs = library_dirs,
+    extra_link_args = extra_link_args,
+    define_macros = define_macros,
+    libraries=['aubio'])
 
 classifiers = [
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Multimedia :: Sound/Audio :: Analysis',
-        'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: C',
-        'Programming Language :: Python',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        ]
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Science/Research',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Multimedia :: Sound/Audio :: Analysis',
+    'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis',
+    'Operating System :: POSIX',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows',
+    'Programming Language :: C',
+    'Programming Language :: Python',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    ]
 
 distrib = setup(name='aubio',
-        version = __version__,
-        packages = ['aubio'],
-        ext_modules = [aubio_extension],
-        description = 'interface to the aubio library',
-        long_description = 'interface to the aubio library',
-        license = 'GNU/GPL version 3',
-        author = 'Paul Brossier',
-        author_email = 'piem@aubio.org',
-        maintainer = 'Paul Brossier',
-        maintainer_email = 'piem@aubio.org',
-        url = 'http://aubio.org/',
-        platforms = 'any',
-        classifiers = classifiers,
-        )
-
+    version = __version__,
+    packages = ['aubio'],
+    ext_modules = [aubio_extension],
+    description = 'interface to the aubio library',
+    long_description = 'interface to the aubio library',
+    license = 'GNU/GPL version 3',
+    author = 'Paul Brossier',
+    author_email = 'piem@aubio.org',
+    maintainer = 'Paul Brossier',
+    maintainer_email = 'piem@aubio.org',
+    url = 'http://aubio.org/',
+    platforms = 'any',
+    classifiers = classifiers,
+    )
