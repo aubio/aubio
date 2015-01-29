deb1fd481a56f1ef979ff1d70b0e6ac6252a13ab
Paul Brossier <piem@piem.org>
2015-01-29 16:25:38 +0100

    src/tempo/tempo.h: add aubio_tempo_get_silence and aubio_tempo_get_threshold

--- src/tempo/tempo.c
+++ src/tempo/tempo.c
@@ -150,10 +150,18 @@ uint_t aubio_tempo_set_silence(aubio_tempo_t * o, smpl_t silence) {
   return AUBIO_OK;
 }
 
+smpl_t aubio_tempo_get_silence(aubio_tempo_t * o) {
+  return o->silence;
+}
+
 uint_t aubio_tempo_set_threshold(aubio_tempo_t * o, smpl_t threshold) {
   o->threshold = threshold;
   aubio_peakpicker_set_threshold(o->pp, o->threshold);
   return AUBIO_OK;
+}
+
+smpl_t aubio_tempo_get_threshold(aubio_tempo_t * o) {
+  return o->threshold;
 }
 
 /* Allocate memory for an tempo detection */
