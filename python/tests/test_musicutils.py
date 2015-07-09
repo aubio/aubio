5e394ecc78197135efbec3d64ff9b4d3eec61358
Paul Brossier <piem@piem.org>
2015-07-10 00:16:19 +0200

    tests/test_musicutils.py: improve test

--- python/tests/test_musicutils.py
+++ python/tests/test_musicutils.py
@@ -8,6 +8,22 @@ class aubio_window(TestCase):
     def test_accept_name_and_size(self):
         window("default", 1024)
 
+    def test_fail_name_not_string(self):
+        try:
+            window(10, 1024)
+        except ValueError, e:
+            pass
+        else:
+            self.fail('non-string window type does not raise a ValueError')
+
+    def test_fail_size_not_int(self):
+        try:
+            window("default", "default")
+        except ValueError, e:
+            pass
+        else:
+            self.fail('non-integer window length does not raise a ValueError')
+
 if __name__ == '__main__':
     from unittest import main
     main()
