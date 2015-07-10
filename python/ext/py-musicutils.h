665b711fd9325b8c1bf1170b8f038d31adcdf5df
Paul Brossier <piem@piem.org>
2015-07-10 02:25:47 +0200

    ext/py-musicutils.h: add doc for level_lin and db_spl

--- python/ext/py-musicutils.h
+++ python/ext/py-musicutils.h
@@ -15,4 +15,34 @@ static char Py_aubio_window_doc[] = ""
 
 PyObject * Py_aubio_window(PyObject *self, PyObject *args);
 
+static char Py_aubio_level_lin_doc[] = ""
+"level_lin(fvec) -> fvec\n"
+"\n"
+"Compute sound level on a linear scale.\n"
+"\n"
+"This gives the average of the square amplitudes.\n"
+"\n"
+"Example\n"
+"-------\n"
+"\n"
+">>> level_Lin(numpy.ones(1024))\n"
+"1.0";
+
+PyObject * Py_aubio_level_lin(PyObject *self, PyObject *args);
+
+static char Py_aubio_db_spl_doc[] = ""
+"Compute sound pressure level (SPL) in dB\n"
+"\n"
+"This quantity is often wrongly called 'loudness'.\n"
+"\n"
+"This gives ten times the log10 of the average of the square amplitudes.\n"
+"\n"
+"Example\n"
+"-------\n"
+"\n"
+">>> db_spl(numpy.ones(1024))\n"
+"1.0";
+
+PyObject * Py_aubio_db_spl(PyObject *self, PyObject *args);
+
 #endif /* _PY_AUBIO_MUSICUTILS_H_ */
