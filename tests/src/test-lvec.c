7a62fcb6cbbdf7e0478a4911feda8d21c1b9b1b7
Paul Brossier <piem@piem.org>
2016-04-30 16:57:16 +0200

    tests/src/test-lvec.c: use AUBIO_LSMP_FMT

--- tests/src/test-lvec.c
+++ tests/src/test-lvec.c
@@ -6,7 +6,7 @@ int main (void)
   uint_t win_s = 32; // window size
   lvec_t * sp = new_lvec (win_s); // input buffer
   lvec_set_sample (sp, 2./3., 0);
-  PRINT_MSG("%lf\n", lvec_get_sample (sp, 0));
+  PRINT_MSG(AUBIO_LSMP_FMT "\n", lvec_get_sample (sp, 0));
   lvec_print (sp);
   lvec_ones (sp);
   lvec_print (sp);
