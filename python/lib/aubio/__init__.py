b96a7b88da69d30b3617fe6b25e91058a5c6189b
Paul Brossier <piem@piem.org>
2016-05-11 09:40:04 +0200

    python/lib/aubio/__init__.py: make sure length is not zero and float_type is imported

--- python/lib/aubio/__init__.py
+++ python/lib/aubio/__init__.py
@@ -2,6 +2,7 @@
 
 import numpy
 from ._aubio import *
+from ._aubio import float_type
 from .midiconv import *
 from .slicing import *
 
@@ -10,6 +11,8 @@ class fvec(numpy.ndarray):
 
     def __new__(cls, input_arg=1024, **kwargs):
         if isinstance(input_arg, int):
+            if input_arg == 0:
+                raise ValueError("vector length of 1 or more expected")
             return numpy.zeros(input_arg, dtype=float_type, **kwargs)
         else:
             return numpy.array(input_arg, dtype=float_type, **kwargs)
