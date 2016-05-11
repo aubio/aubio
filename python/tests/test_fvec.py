44312de69a25c21d2ef48471ff7f09c1c1ce18c6
Paul Brossier <piem@piem.org>
2016-05-11 11:00:22 +0200

    python/tests/test_fvec.py: cope with accumulated errors

--- python/tests/test_fvec.py
+++ python/tests/test_fvec.py
@@ -97,7 +97,7 @@ class aubio_alpha_norm(TestCase):
         x = np.random.rand(1024).astype(float_type)
         alpha = np.random.rand() * 5.
         x_alpha_norm = (np.sum(np.abs(x)**alpha)/len(x))**(1/alpha)
-        assert_almost_equal(alpha_norm(x, alpha), x_alpha_norm)
+        assert_almost_equal(alpha_norm(x, alpha), x_alpha_norm, decimal = 5)
 
 class aubio_zero_crossing_rate_test(TestCase):
 
