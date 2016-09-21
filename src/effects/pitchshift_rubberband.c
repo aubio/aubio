25a19c2d1f811b89f4e39138a338a16b4255c1eb
Paul Brossier <piem@piem.org>
2016-09-21 17:36:50 +0200

    src/effects/pitchshift.h: split implementations in two files, move option parsing to rubberband_utils.c

--- src/effects/pitchshift_rubberband.c
+++ src/effects/pitchshift_rubberband.c
@@ -19,21 +19,14 @@
 */
 
 #include "config.h"
+
+#ifdef HAVE_RUBBERBAND
+
 #include "aubio_priv.h"
 #include "fvec.h"
 #include "effects/pitchshift.h"
 
-#ifdef HAVE_RUBBERBAND
-
 #include "rubberband/rubberband-c.h"
-
-// check rubberband is 1.8.1, warn if 1.3
-#if !((RUBBERBAND_API_MAJOR_VERSION >= 2) && \
-    (RUBBERBAND_API_MINOR_VERSION >= 5))
-#warning RubberBandOptionDetectorSoft not available, \
- please upgrade rubberband to version 1.8.1 or higher
-#define RubberBandOptionDetectorSoft 0x00000000
-#endif
 
 /** generic pitch shifting structure */
 struct _aubio_pitchshift_t
@@ -45,6 +38,8 @@ struct _aubio_pitchshift_t
   RubberBandState rb;
   RubberBandOptions rboptions;
 };
+
+extern RubberBandOptions aubio_get_rubberband_opts(const char_t *mode);
 
 aubio_pitchshift_t *
 new_aubio_pitchshift (const char_t * mode,
@@ -61,43 +56,13 @@ new_aubio_pitchshift (const char_t * mode,
     goto beach;
   }
 
-  p->rboptions = RubberBandOptionProcessRealTime;
-
-  if ( strcmp(mode,"crispness:0") == 0 ) {
-    p->rboptions |= RubberBandOptionTransientsSmooth;
-    p->rboptions |= RubberBandOptionWindowLong;
-    p->rboptions |= RubberBandOptionPhaseIndependent;
-  } else if ( strcmp(mode, "crispness:1") == 0 ) {
-    p->rboptions |= RubberBandOptionDetectorSoft;
-    p->rboptions |= RubberBandOptionTransientsSmooth;
-    p->rboptions |= RubberBandOptionWindowLong;
-    p->rboptions |= RubberBandOptionPhaseIndependent;
-  } else if ( strcmp(mode, "crispness:2") == 0 ) {
-    p->rboptions |= RubberBandOptionTransientsSmooth;
-    p->rboptions |= RubberBandOptionPhaseIndependent;
-  } else if ( strcmp(mode, "crispness:3") == 0 ) {
-    p->rboptions |= RubberBandOptionTransientsSmooth;
-  } else if ( strcmp(mode, "crispness:4") == 0 ) {
-    // same as "default"
-  } else if ( strcmp(mode, "crispness:5") == 0 ) {
-    p->rboptions |= RubberBandOptionTransientsCrisp;
-  } else if ( strcmp(mode, "crispness:6") == 0 ) {
-    p->rboptions |= RubberBandOptionTransientsCrisp;
-    p->rboptions |= RubberBandOptionWindowShort;
-    p->rboptions |= RubberBandOptionPhaseIndependent;
-  } else if ( strcmp(mode, "default") == 0 ) {
-    // nothing to do
-  } else {
-    AUBIO_ERR("pitchshift: unknown pitch shifting method %s\n", mode);
+  p->rboptions = aubio_get_rubberband_opts(mode);
+  if (p->rboptions < 0) {
+    AUBIO_ERR("timestretch: unknown pitch shifting method %s\n", mode);
     goto beach;
   }
   //AUBIO_MSG("pitchshift: using pitch shifting method %s\n", mode);
 
-  //p->rboptions |= RubberBandOptionTransientsCrisp;
-  //p->rboptions |= RubberBandOptionWindowStandard;
-  //p->rboptions |= RubberBandOptionSmoothingOff;
-  //p->rboptions |= RubberBandOptionFormantShifted;
-  //p->rboptions |= RubberBandOptionPitchHighConsistency;
   p->rb = rubberband_new(samplerate, 1, p->rboptions, 1., p->pitchscale);
   rubberband_set_max_process_size(p->rb, p->hopsize);
   //rubberband_set_debug_level(p->rb, 10);
@@ -186,51 +151,4 @@ aubio_pitchshift_do (aubio_pitchshift_t * p, const fvec_t * in, fvec_t * out)
   }
 }
 
-#else
-
-// TODO fallback pitch shifting implementation
-
-struct _aubio_pitchshift_t
-{
-  void *dummy;
-};
-
-void aubio_pitchshift_do (aubio_pitchshift_t * o UNUSED, const fvec_t * in UNUSED,
-    fvec_t * out UNUSED) {
-}
-
-void del_aubio_pitchshift (aubio_pitchshift_t * o UNUSED) {
-}
-
-aubio_pitchshift_t *new_aubio_pitchshift (const char_t * method UNUSED,
-    smpl_t pitchscale UNUSED, uint_t hop_size UNUSED, uint_t samplerate UNUSED)
-{
-  AUBIO_ERR ("aubio was not compiled with rubberband\n");
-  return NULL;
-}
-
-uint_t aubio_pitchshift_set_pitchscale (aubio_pitchshift_t * o UNUSED, smpl_t pitchscale UNUSED)
-{
-  return AUBIO_FAIL;
-}
-
-smpl_t aubio_pitchshift_get_pitchscale (aubio_pitchshift_t * o UNUSED)
-{
-  return 1.;
-}
-
-uint_t aubio_pitchshift_set_transpose (aubio_pitchshift_t * o UNUSED, smpl_t transpose UNUSED) {
-  return AUBIO_FAIL;
-}
-
-smpl_t aubio_pitchshift_get_transpose (aubio_pitchshift_t * o UNUSED) {
-  return 0.;
-}
-
-uint_t aubio_pitchshift_get_latency (aubio_pitchshift_t * o UNUSED) {
-  return 0.;
-}
-
-// end of dummy implementation
-
-#endif /* HAVE_RUBBERBAND */
+#endif
---
+++ src/effects/rubberband_utils.c
@@ -0,0 +1,58 @@
+
+
+#include "config.h"
+#include "aubio_priv.h"
+
+#ifdef HAVE_RUBBERBAND
+
+#include "rubberband/rubberband-c.h"
+
+// check rubberband is 1.8.1, warn if 1.3
+#if !((RUBBERBAND_API_MAJOR_VERSION >= 2) && \
+    (RUBBERBAND_API_MINOR_VERSION >= 5))
+#warning RubberBandOptionDetectorSoft not available, \
+ please upgrade rubberband to version 1.8.1 or higher
+#define RubberBandOptionDetectorSoft 0x00000000
+#endif
+
+RubberBandOptions aubio_get_rubberband_opts(const char_t *mode)
+{
+  RubberBandOptions rboptions = RubberBandOptionProcessRealTime;
+
+  if ( strcmp(mode,"crispness:0") == 0 ) {
+    rboptions |= RubberBandOptionTransientsSmooth;
+    rboptions |= RubberBandOptionWindowLong;
+    rboptions |= RubberBandOptionPhaseIndependent;
+  } else if ( strcmp(mode, "crispness:1") == 0 ) {
+    rboptions |= RubberBandOptionDetectorSoft;
+    rboptions |= RubberBandOptionTransientsSmooth;
+    rboptions |= RubberBandOptionWindowLong;
+    rboptions |= RubberBandOptionPhaseIndependent;
+  } else if ( strcmp(mode, "crispness:2") == 0 ) {
+    rboptions |= RubberBandOptionTransientsSmooth;
+    rboptions |= RubberBandOptionPhaseIndependent;
+  } else if ( strcmp(mode, "crispness:3") == 0 ) {
+    rboptions |= RubberBandOptionTransientsSmooth;
+  } else if ( strcmp(mode, "crispness:4") == 0 ) {
+    // same as "default"
+  } else if ( strcmp(mode, "crispness:5") == 0 ) {
+    rboptions |= RubberBandOptionTransientsCrisp;
+  } else if ( strcmp(mode, "crispness:6") == 0 ) {
+    rboptions |= RubberBandOptionTransientsCrisp;
+    rboptions |= RubberBandOptionWindowShort;
+    rboptions |= RubberBandOptionPhaseIndependent;
+  } else if ( strcmp(mode, "default") == 0 ) {
+    // nothing to do
+  } else {
+    // failed parsing option string
+    return -1;
+  }
+  // other options to include
+  //p->rboptions |= RubberBandOptionWindowStandard;
+  //p->rboptions |= RubberBandOptionSmoothingOff;
+  //p->rboptions |= RubberBandOptionFormantShifted;
+  //p->rboptions |= RubberBandOptionPitchHighConsistency;
+  return rboptions;
+}
+
+#endif
