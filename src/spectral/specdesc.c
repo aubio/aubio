2763582da4af10d09edd4bb3f0fd91579cf396a3
Paul Brossier <piem@piem.org>
2016-09-23 00:06:45 +0200

    src/spectral/specdesc.c: return NULL if wrong mode asked

--- src/spectral/specdesc.c
+++ src/spectral/specdesc.c
@@ -273,8 +273,9 @@ new_aubio_specdesc (const char_t * onset_mode, uint_t size){
   else if (strcmp (onset_mode, "default") == 0)
       onset_type = aubio_onset_default;
   else {
-      AUBIO_ERR("unknown spectral descriptor type %s, using default.\n", onset_mode);
-      onset_type = aubio_onset_default;
+      AUBIO_ERR("unknown spectral descriptor type %s\n", onset_mode);
+      AUBIO_FREE(o);
+      return NULL;
   }
   switch(onset_type) {
     /* for both energy and hfc, only fftgrain->norm is required */
