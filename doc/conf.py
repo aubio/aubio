562e9080d06abb5b6c1c25acf79794adeaf01b92
Paul Brossier <piem@piem.org>
2016-12-16 13:01:49 +0100

    doc/conf.py: remove hardcoded path

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../../python/build/lib.macosx-10.6-intel-2.7'))
+#sys.path.insert(0, os.path.abspath('../../python/build/...'))
 
 # -- General configuration -----------------------------------------------------
 
