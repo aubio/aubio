c60dba1d3af8c34b919f8003d8885465c9998a65
Aybe <aybe@users.noreply.github.com>
2017-11-14 00:15:59 +0100

    Added config.h

---
+++ vs2017/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define HAVE_STDLIB_H
+#define HAVE_STDIO_H
+#define HAVE_MATH_H
+#define HAVE_STRING_H
+#define HAVE_LIMITS_H
+#define HAVE_STDARG_H
+#define HAVE_C99_VARARGS_MACROS
