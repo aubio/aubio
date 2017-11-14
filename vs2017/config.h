76fab2b64290f36f13373da55d0247b54becbd7a
Aybe <aybe@users.noreply.github.com>
2017-11-14 02:20:16 +0100

    Enhancing config a bit ...

--- vs2017/config.h
+++ vs2017/config.h
@@ -1,12 +1,19 @@
 #pragma once
 
 #define HAVE_STDLIB_H
+
 #define HAVE_STDIO_H
+
 #define HAVE_MATH_H
+
 #define HAVE_STRING_H
+
 #define HAVE_LIMITS_H
+
 #define HAVE_STDARG_H
+
 #define HAVE_C99_VARARGS_MACROS
-#define HAVE_WIN_HACKS
+
+#define HAVE_WIN_HACKS // TODO once we're sure this works, remove the template-generated files by VS
 
 #define _CRT_SECURE_NO_WARNINGS
