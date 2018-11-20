35f7e0597087fa0a196036925b0773fc39dd14ce
Paul Brossier <piem@piem.org>
2018-11-20 22:34:26 +0100

    [py] filterbank power and norm are floats

--- python/ext/py-filterbank.c
+++ python/ext/py-filterbank.c
@@ -268,9 +268,9 @@ Py_filterbank_get_coeffs (Py_filterbank * self, PyObject *unused)
 static PyObject *
 Py_filterbank_set_power(Py_filterbank *self, PyObject *args)
 {
-  uint_t power;
+  smpl_t power;
 
-  if (!PyArg_ParseTuple (args, "I", &power)) {
+  if (!PyArg_ParseTuple (args, "f", &power)) {
     return NULL;
   }
   if(aubio_filterbank_set_power (self->o, power)) {
@@ -291,12 +291,12 @@ Py_filterbank_set_power(Py_filterbank *self, PyObject *args)
 static PyObject *
 Py_filterbank_set_norm(Py_filterbank *self, PyObject *args)
 {
-  uint_t playing;
+  smpl_t norm;
 
-  if (!PyArg_ParseTuple (args, "I", &playing)) {
+  if (!PyArg_ParseTuple (args, "f", &norm)) {
     return NULL;
   }
-  if(aubio_filterbank_set_norm (self->o, playing)) {
+  if(aubio_filterbank_set_norm (self->o, norm)) {
     if (PyErr_Occurred() == NULL) {
       PyErr_SetString (PyExc_ValueError,
           "error running filterbank.set_power");
