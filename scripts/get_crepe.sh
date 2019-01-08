f335a9baaa21d2ac8d7c0fc2897db36112c48a62
Paul Brossier <piem@piem.org>
2019-01-08 16:40:03 +0100

    [scripts] script to fetch crepe model from upstream repo

---
+++ scripts/get_crepe.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+CREPEURL=https://github.com/marl/crepe/blob/models/model-tiny.h5.bz2?raw=true
+MODELTARGET=crepe-model-tiny.h5
+SHA512=91df10316092de0d9c35ae0eaa8f6cceb49fb01f54dc74f9f1196f7f569a3f885242d1abded56c9825180552a602d9e76f2021d7cfb55cfbefb2f84c8a9f4715
+
+
+function checkmodelsum () {
+  (echo $SHA512  $MODELTARGET | sha512sum -c - ) || (echo $MODELTARGET checksum did not match?!; exit 1)
+}
+
+function fetchmodel () {
+  wget -qO- $CREPEURL | bunzip2 - > $MODELTARGET
+}
+
+( [ -f "$MODELTARGET" ] || fetchmodel ) && checkmodelsum
