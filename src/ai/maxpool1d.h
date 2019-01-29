514a87cf5efb59b04b895923d9b48cd8ea8becbc
Paul Brossier <piem@piem.org>
2019-01-29 02:59:00 +0100

    [maxpool1d] add basic documentation

--- src/ai/maxpool1d.h
+++ src/ai/maxpool1d.h
@@ -21,21 +21,56 @@
 #ifndef AUBIO_MAXPOOL1D_H
 #define AUBIO_MAXPOOL1D_H
 
+/** \file
+
+  Max pooling layer (1D)
+
+*/
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/** maxpool1d layer */
 typedef struct _aubio_maxpool1d_t aubio_maxpool1d_t;
 
+/** create a new maxpool1d layer
+
+  \param pool_size  size of the pooling windows
+
+  \return new ::aubio_maxpool2d_t layer
+
+*/
 aubio_maxpool1d_t *new_aubio_maxpool1d(uint_t pool_size[1]);
 
+/** get output shape
+
+  \param t      layer
+  \param input  input tensor
+  \param shape  output shape
+
+  \return 0 on success, non-zero otherwise
+
+*/
 uint_t aubio_maxpool1d_get_output_shape(aubio_maxpool1d_t *t,
         aubio_tensor_t *input, uint_t *shape);
 
+/** compute layer output
+
+  \param    t               layer
+  \param    input_tensor    input tensor
+  \param    output_tensor   output tensor
+
+*/
 void aubio_maxpool1d_do(aubio_maxpool1d_t *t,
         aubio_tensor_t *input_tensor,
-        aubio_tensor_t *activations);
+        aubio_tensor_t *output_tensor);
 
+/** destroy layer
+
+  \param t      layer to destroy
+
+*/
 void del_aubio_maxpool1d(aubio_maxpool1d_t *t);
 
 #ifdef __cplusplus
